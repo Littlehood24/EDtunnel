595fd42a4be2dca36c0692428aafeb1f602db476
Littlehood24 <littlehood24@gmail.com>
2024-06-03 00:41:57 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -5,7 +5,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = 'e2ad781a-0927-4061-8324-2cde24cd982f';
 
-const พร็อกซีไอพีs = ['36.95.213.27'];
+const พร็อกซีไอพีs = ['36.68.54.44'];
 
 // if you want to use ipv6 or single พร็อกซีไอพี, please add comment at this line and remove comment at the next line
 let พร็อกซีไอพี = พร็อกซีไอพีs[Math.floor(Math.random() * พร็อกซีไอพีs.length)];
